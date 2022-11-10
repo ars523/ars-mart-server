0ae96ac54be21c99f42e032924d94b6e52f192d0
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-11-10 17:41:07 +0600

    updated

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/upload', uploadRouter)
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder up in production
   // app.use(express.static(path.join(__dirname, '../frontend/build')))
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (_, res) => {
       res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
