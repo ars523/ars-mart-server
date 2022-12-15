3d273214d0335dc6ff3e6414bf1b86629804129d
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-12-15 10:41:01 +0600

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const dotevn = require('dotenv').config()
 const path = require('path')
 const data = require('./data')
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDb = require('./config/db')
 const seedRouter = require('./routes/seedRoutes')
 const productRouter = require('./routes/productRoutes')
@@ -13,35 +13,20 @@ const uploadRouter = require('./routes/uploadRoutes')
 connectDb()
 
 const app = express()
-const Port = 5000
+const Port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(cors({origin:['http://localhost:3000', 'https://ars-mart.onrender.com']}));
+app.use(express.urlencoded({ extended: false }))
+app.use(cors({ origin: ['http://localhost:3000', 'https://ars-mart.onrender.com'] }));
 app.use('/api/seed', seedRouter)
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
 app.use('/api/upload', uploadRouter)
 
-
-// if (process.env.NODE_ENV === 'production') {
-//   //*Set static folder up in production
-//   // app.use(express.static(path.join(__dirname, '../frontend/build')))
-//   app.use(express.static('frontend/build'));
-
-//   app.get('*', (_, res) => {
-//       res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-//   })
-// } else {
-//   app.get('/', (_, res) => {
-//       res.status(200).json({ message: 'Welcome to the ars-mart API' })
-//   })
-// }
-
-  app.get('/', (_, res) => {
-      res.status(200).json({ message: 'Welcome to the ars-mart API' })
-  })
+app.get('/', (_, res) => {
+  res.status(200).json({ message: 'Welcome to the ars-mart API' })
+})
 
 app.use(errorHandler)
 
